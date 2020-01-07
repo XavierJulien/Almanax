6fa0aad6cfb40475c00b5680e5badd0a78860bff
Julienxavier774@gmail.com <Julienxavier774@gmail.com>
2020-01-07 06:17:51 +0100

    Ajout de la progression

--- app/src/main/java/com/jxavier/almanax/MainActivity.java
+++ app/src/main/java/com/jxavier/almanax/MainActivity.java
@@ -2,6 +2,7 @@ package com.jxavier.almanax;
 
 import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
@@ -13,9 +14,15 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.bitmap.CenterCrop;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import android.text.SpannableString;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.RelativeSizeSpan;
 import android.util.Log;
 import android.view.View;
 
@@ -35,7 +42,6 @@ import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.w3c.dom.Text;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -44,9 +50,14 @@ import static android.Manifest.permission.ACCESS_NETWORK_STATE;
 import static android.Manifest.permission.INTERNET;
 import static android.Manifest.permission.READ_EXTERNAL_STORAGE;
 
-public class MainActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
     private Context context = MainActivity.this;
+    //-----------------------------------------------
+    // Date
+    //-----------------------------------------------
+    Calendar calendar = Calendar.getInstance();
+    SimpleDateFormat sdf = new SimpleDateFormat("MM-dd");
+    final String  date = sdf.format(calendar.getTime());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,12 +66,13 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Progression mise à jour !", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+                fab.setImageResource(R.drawable.ic_star_fill);
             }
         });
 
@@ -73,20 +85,56 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+
+        //-----------------------------------------------
+        // Permissions
+        //-----------------------------------------------
         requestStoragePermission();
-        final ImageView imageView = findViewById(R.id.photo_almanax);
-        final TextView nameView = findViewById(R.id.textView2);
+
+        //-----------------------------------------------
+        // References
+        //-----------------------------------------------
+        final ImageView objectIDView = findViewById(R.id.objectid);
+        final ImageView bossView = findViewById(R.id.boss);
+        final TextView nameView = findViewById(R.id.name);
+        final TextView offeringView = findViewById(R.id.offering);
+        final TextView bonusTitleView = findViewById(R.id.bonustitle);
+        final TextView bonusDescView = findViewById(R.id.bonusdesc);
+        final ImageView monthView = findViewById(R.id.month);
+        final ImageView zodiacView = findViewById(R.id.zodiac);
+        Glide.with(context)
+                .load("https://staticns.ankama.com/krosmoz/img/uploads/month/7/all_128_128.png")
+                .into(monthView);
+        Glide.with(context)
+                .load("https://staticns.ankama.com/krosmoz/img/uploads/zodiac/14/all_128_128.png")
+                .apply(RequestOptions.bitmapTransform(new RoundedCorners(14)))
+                .into(zodiacView);
+
+        String s= "18 Descendre";
+        SpannableString ss1=  new SpannableString(s);
+        ss1.setSpan(new RelativeSizeSpan(2f), 0,2, 0); // set size
+        ss1.setSpan(new ForegroundColorSpan(Color.parseColor("#efbf31")), 0, s.length(),0);// set color
+        TextView tv= (TextView) findViewById(R.id.date_text);
+        tv.setText(ss1);
+
+        //-----------------------------------------------
+        // Update image
+        //-----------------------------------------------
         ConnectivityManager connectivityManager = (ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
         if(connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_MOBILE).getState() == NetworkInfo.State.CONNECTED ||
-                connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
-            //we are connected to a network
-            setTodayAlmanax(imageView,nameView);
+           connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI).getState() == NetworkInfo.State.CONNECTED) {
+            setTodayAlmanax(bossView,objectIDView,nameView,offeringView,bonusTitleView,bonusDescView);
         }else{
             Glide.with(context)
                     .load(R.drawable.picto_asset_dofus)
-                    .into(imageView);
+                    .into(bossView);
+            Glide.with(context)
+                    .load(R.drawable.picto_asset_dofus)
+                    .into(objectIDView);
             nameView.setText("Not Connected. Turn on Wifi");
         }
+        setDateBackground();
+
     }
 
     @Override
@@ -155,11 +203,13 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    private void setTodayAlmanax(final ImageView imageView, final TextView textView){
-            Calendar calendar = Calendar.getInstance();
-            SimpleDateFormat sdf = new SimpleDateFormat("MM-dd");
-            final String  date = sdf.format(calendar.getTime());
-            String url = "https://api.jsonbin.io/b/5def81331c19843d88e9a4ca";
+    private void setTodayAlmanax(final ImageView bossView,
+                                 final ImageView objectIDView,
+                                 final TextView nameView,
+                                 final TextView offeringView,
+                                 final TextView bonusTitleView,
+                                 final TextView bonusDescView){
+            String url = "https://api.jsonbin.io/b/5def81331c19843d88e9a4ca/1";
             // Instantiate the RequestQueue.
             RequestQueue queue = Volley.newRequestQueue(this);
             JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
@@ -171,13 +221,15 @@ public class MainActivity extends AppCompatActivity
                                 JSONObject day = dofus.getJSONObject(date);
                                 Glide.with(context)
                                         .load("https://static.ankama.com/dofus/www/game/items/200/"+day.getString("objectID")+".png")
-                                        .into(imageView);
-                                textView.setText(day.getString("name"));
-                            /*res.setDate(date);
-                            res.setOffering(day.getString("offering"));
-                            res.setBonusTitle(day.getString("bonusTitle"));
-                            res.setBonusDescription(day.getString("bonusDescription"));
-                            res.setObjectID(day.getString("objectID"));*/
+                                        .into(objectIDView);
+                                Glide.with(context)
+                                        .load("https://staticns.ankama.com/krosmoz/img/uploads/event/136/boss_all_96_128.png")
+                                        .error(R.drawable.picto_asset_dofus)
+                                        .into(bossView);
+                                nameView.setText("Quête : Offrande à "+day.getString("name"));
+                                offeringView.setText("Récupérer "+day.getString("offering")+" et rapporter l'offrande à Théodoran Ax");
+                                bonusTitleView.setText(day.getString("bonusTitle"));
+                                bonusDescView.setText(day.getString("bonusDescription"));
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
@@ -192,5 +244,88 @@ public class MainActivity extends AppCompatActivity
                         }
                     });
             queue.add(request);
+    }
+
+    private void setDateBackground(){
+        //final ConstraintLayout background = findViewById(R.id.background);
+        SimpleDateFormat sdf = new SimpleDateFormat("MM");
+        final String  month = sdf.format(calendar.getTime());
+        Log.d("Date", month);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        View hView =  navigationView.getHeaderView(0);
+        TextView almanax_season = hView.findViewById(R.id.almanax_season);
+        ImageView almanax_icon = hView.findViewById(R.id.almanax_icon);
+        switch(month){
+            case "01": {
+                almanax_season.setText("Winter");
+                almanax_icon.setImageResource(R.drawable.winter_season);
+                break;
+            }
+            case "02": {
+                almanax_season.setText("Winter");
+                almanax_icon.setImageResource(R.drawable.winter_season);
+                break;
+            }
+            case "03": {
+                almanax_season.setText("Spring");
+                almanax_icon.setImageResource(R.drawable.spring_season);
+
+                break;
+            }
+            case "04": {
+                almanax_season.setText("Spring");
+                almanax_icon.setImageResource(R.drawable.spring_season);
+
+                break;
+            }
+            case "05": {
+                almanax_season.setText("Spring");
+                almanax_icon.setImageResource(R.drawable.spring_season);
+
+                break;
+            }
+            case "06": {
+                almanax_season.setText("Summer");
+                almanax_icon.setImageResource(R.drawable.summer_season);
+
+                break;
+            }
+            case "07": {
+                almanax_season.setText("Summer");
+                almanax_icon.setImageResource(R.drawable.summer_season);
+
+                break;
+            }
+            case "08": {
+                almanax_season.setText("Summer");
+                almanax_icon.setImageResource(R.drawable.summer_season);
+
+                break;
+            }
+            case "09": {
+                almanax_season.setText("Autumn");
+                almanax_icon.setImageResource(R.drawable.autumn_season);
+
+                break;
+            }
+            case "10": {
+                almanax_season.setText("Autumn");
+                almanax_icon.setImageResource(R.drawable.autumn_season);
+
+                break;
+            }
+            case "11": {
+                almanax_season.setText("Autumn");
+                almanax_icon.setImageResource(R.drawable.autumn_season);
+
+                break;
+            }
+            case "12": {
+                almanax_season.setText("Winter");
+                almanax_icon.setImageResource(R.drawable.winter_season);
+                break;
+            }
+            default: throw new Error("Bug check date.");
+        }
     }
 }
